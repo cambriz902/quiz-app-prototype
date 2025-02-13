6184f4d5f58f4b479518c12b6e3ec375498b76ab
christian ambriz <christianambriz@christians-MacBook-Air.local>
2025-02-13 15:27:04 -0800

    remove unused comments

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,6 @@ import type { Config } from 'jest'
 import nextJest from 'next/jest.js'
  
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
 })
  
@@ -11,8 +10,6 @@ const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  // testEnvironment: 'jest-environment-jsdom',
 }
  
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 export default createJestConfig(config)
