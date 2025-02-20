c2bcde66aabcf9c994d41cb1b05a55933e6ce208
christian ambriz <christianambriz@christians-MacBook-Air.local>
2025-02-20 13:58:41 -0800

    fix auth issues

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth.config";
 import { User } from "@prisma/client";
 import prisma from "@/lib/prisma";
 
