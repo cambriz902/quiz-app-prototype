644dd5515ac7a1299f30b2cad901cd935d7eea4a
christian ambriz <christianambriz@christians-MacBook-Air.local>
2025-03-02 13:23:32 -0800

    fix import

--- app/api/agent/route.ts
+++ app/api/agent/route.ts
@@ -1,14 +1,14 @@
-import { NextResponse, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-import { ApiMessage, OpenAIGeneralHelperFormat } from '@/types/openAI';
+import { ApiMessage } from '@/types/openAI';
 import { promptUserForQuizSearch } from "@/lib/services/openaiService";
 
 interface PostData {
 	messages: ApiMessage[];
 }
 
-export async function POST(request: Request): NextResponse<OpenAIGeneralHelperFormat> {
-	const data: PostDataType = await request.json();
+export async function POST(request: Request){
+	const data: PostData = await request.json();
 	const response = await promptUserForQuizSearch(data.messages);
 	return NextResponse.json(response)
 }
